03d3250c86d8976673498aa7ca8b48bbec15d603
Simran Spiller <simran.spiller@gmail.com>
2022-04-06 22:22:33 +0200

    WIP

--- lexers/a/aql.go
+++ lexers/a/aql.go
@@ -1,19 +1,18 @@
 package a
 
 import (
-	. "github.com/alecthomas/chroma" // nolint
-	"github.com/alecthomas/chroma/lexers/internal"
+	. "github.com/alecthomas/chroma/v2" // nolint
 )
 
 // ArangoDB Query Language (AQL) lexer.
-var Aql = internal.Register(MustNewLexer( // nolint: forbidigo
+var Aql = Register(MustNewLexer( // nolint: forbidigo
 	&Config{
 		Name:      "AQL",
 		Aliases:   []string{"aql"},
 		Filenames: []string{"*.aql"},
 		MimeTypes: []string{"application/x-aql"},
-		DotAll:    true, // ???
-		EnsureNL:  true,
+		DotAll: true,
+		EnsureNL: true,
 		CaseInsensitive: true,
 		// Generally case-insensitive (keywords) but not in case of pseudo variables
 	},
@@ -62,25 +61,34 @@ Comments: single, multi
 */
 
 var AqlRules = Rules{
-	"commentsandwhitespace": {
-		{`\s+`, Text, nil},
-		{`//.*?\n`, CommentSingle, nil}, // DotAll?
-		{`/\*`, CommentMultiline, Push("multiline-comment")},
-	},
 	"multiline-comment": {
 		{`\*/`, CommentMultiline, Pop(1)},
-	}
-	"slashstartsregex": {
-		Include("commentsandwhitespace"),
-		{`/(\\.|[^[/\\\n]|\[(\\.|[^\]\\\n])*])+/([gimuy]+\b|\B)`, LiteralStringRegex, Pop(1)},
-		{`(?=/)`, Text, Push("#pop", "badregex")},
-		Default(Pop(1)),
+		{`.+`, CommentMultiline, nil},
 	},
-	"badregex": {
-		{`\n`, Text, Pop(1)},
+	"double-quote": {
+		{`\\"`, LiteralStringDouble, nil},
+		{`"`, LiteralStringDouble, Pop(1)},
+		{`.`, LiteralStringDouble, nil},
+	},
+	"single-quote": {
+		{`\\'`, LiteralStringSingle, nil},
+		{`'`, LiteralStringSingle, Pop(1)},
+		{`.+`, LiteralStringSingle, nil},
+	},
+	"backtick": {
+		{"\\\\`", Name, nil},
+		{"`", Name, Pop(1)},
+		{`.+`, Name, nil},
+	},
+	"forwardtick": {
+		{"\\\\´", Name, nil},
+		{"´", Name, Pop(1)},
+		{`.+`, Name, nil},
 	},
 	"root": {
-		Include("commentsandwhitespace"),
+		{`\s+`, Text, nil},
+		{`//.*?\n`, CommentSingle, nil},
+		{`/\*`, CommentMultiline, Push("multiline-comment")},
 		{`0|[1-9][0-9]*`, LiteralNumberInteger, nil},
 		{`0x[0-9a-f]+`, LiteralNumberHex, nil},
 		{`0b[01]+`, LiteralNumberBin, nil},
@@ -88,27 +96,14 @@ var AqlRules = Rules{
 		{`@@?(_+[a-z0-9]+[a-z0-9_]*|[a-z0-9][a-z0-9_]*)`, NameVariable, nil},
 		{`[,(){}\[\]]`, Punctuation, nil},
 		{`=~|!~|[=!<>]=?|[%?:/*+-]|::|\.\.|&&|\|\|`, Operator, nil},
-		{`(AGGREGATE|ALL|AND|ANY|ASC|COLLECT|DESC|DISTINCT|FALSE|FILTER|FOR|GRAPH|IN|INBOUND|INSERT|INTO|K_PATHS|K_SHORTEST_PATHS|LET|LIKE|LIMIT|NONE|NOT|NULL|OR|OUTBOUND|REMOVE|REPLACE|RETURN|SHORTEST_PATH|SORT|TRUE|UPDATE|UPSERT|WITH|WINDOW)\b`, KeywordReserved, nil},
-		{`(true|false|null)\b`, KeywordConstant, nil}, // remove from keyword list?
-		// WITH or LET? {`()\b`, KeywordDeclaration, Push("slashstartsregex")},
+		{`(AGGREGATE|ALL|AND|ANY|ASC|COLLECT|DESC|DISTINCT|FILTER|FOR|GRAPH|IN|INBOUND|INSERT|INTO|K_PATHS|K_SHORTEST_PATHS|LIKE|LIMIT|NONE|NOT|OR|OUTBOUND|REMOVE|REPLACE|RETURN|SHORTEST_PATH|SORT|UPDATE|UPSERT|WITH|WINDOW)\b`, KeywordReserved, nil},
+		{`LET\b`, KeywordDeclaration, nil}, // also WITH but only at beginning of query
+		{`(true|false|null)\b`, KeywordConstant, nil},
 		// Keyword-like? {`()\b`, Keyword, nil},
 		// aql:: or functions or pseudo variables? {`()\b`, NameBuiltin, nil},
-		{`(?:[$_\p{L}\p{N}]|\\u[a-fA-F0-9]{4})(?:(?:[$\p{L}\p{N}]|\\u[a-fA-F0-9]{4}))*`, NameOther, nil},
-		{`"(\\\\|\\"|[^"])*"`, LiteralStringDouble, nil},
-		{`'(\\\\|\\'|[^'])*'`, LiteralStringSingle, nil},
-		{"`", LiteralStringBacktick, Push("interp")},
-	},
-	"interp": {
-		{"`", LiteralStringBacktick, Pop(1)},
-		{`\\\\`, LiteralStringBacktick, nil},
-		{"\\\\`", LiteralStringBacktick, nil},
-		{"\\\\[^`\\\\]", LiteralStringBacktick, nil},
-		{`\$\{`, LiteralStringInterpol, Push("interp-inside")},
-		{`\$`, LiteralStringBacktick, nil},
-		{"[^`\\\\$]+", LiteralStringBacktick, nil},
-	},
-	"interp-inside": {
-		{`\}`, LiteralStringInterpol, Pop(1)},
-		Include("root"),
+		{`"`, LiteralStringDouble, Push("double-quote")},
+		{`'`, LiteralStringSingle, Push("single-quote")},
+		{"`", Name, Push("backtick")},
+		{"´", Name, Push("forwardtick")},
 	},
 }
